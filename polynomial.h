d3f840b0b63d25839c8a69f6811ad0da00780673
Woo joo Chae <wchae@c1r5s4.42seoul.kr>
2022-04-17 17:37:52 +0900

    init

---
+++ polynomial.h
@@ -0,0 +1,26 @@
+#ifndef _POLYNOMIAL_
+#define _POLYNOMIAL_
+
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct ListNodeType {
+    int coef;
+    int expon;
+    struct ListNodeType *pLink;
+} ListNode;
+
+typedef struct LinkedListType
+{
+	int currentElementCount;
+	ListNode *headerNode;
+    ListNode *tail;
+} LinkedList;
+
+void error(char *message);
+LinkedList *create();
+void    insert_last(LinkedList pList, int coef, int expon);
+void    poly_add(LinkedList *pList1, LinkedList *pList2, LinkedList pList3);
+void    poly_print(LinkedList *pList);
+
+#endif
